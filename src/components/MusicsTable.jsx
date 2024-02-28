fe542858dd19308accc782e33db8f0224074c538
Johan León <64605783+JohanLeon0914@users.noreply.github.com>
2024-02-27 20:56:01 -0500

    agregando el icono del tiempo en componente de tabla

--- src/components/MusicsTable.jsx
+++ src/components/MusicsTable.jsx
@@ -1,6 +1,18 @@
-import Time from "@/icons/Time.astro";
 import { usePlayerStore } from "@/store/playerStore";
 import { Pause, Play } from "@/components/Player";
+const TimeIcon = () => (
+  <svg
+    role="img"
+    height="16"
+    width="16"
+    aria-hidden="true"
+    viewBox="0 0 16 16"
+    fill="currentColor"
+  >
+    <path d="M8 1.5a6.5 6.5 0 1 0 0 13 6.5 6.5 0 0 0 0-13zM0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8z"></path>
+    <path d="M8 3.25a.75.75 0 0 1 .75.75v3.25H11a.75.75 0 0 1 0 1.5H7.25V4A.75.75 0 0 1 8 3.25z"></path>
+  </svg>
+);
 
 const PlaylistTable = ({ songs, playlist }) => {
   const {
@@ -22,6 +34,7 @@ const PlaylistTable = ({ songs, playlist }) => {
           <th className="px-4 py-2 font-light">#</th>
           <th className="px-4 py-2 font-light">Título</th>
           <th className="px-4 py-2 font-light">Álbum</th>
+          <th className="px-4 py-2 font-light"><TimeIcon /></th>
         </tr>
       </thead>
 
