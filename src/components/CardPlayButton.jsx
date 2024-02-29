1505006c00d59885e741ff2181f096f26fd46fe7
Johan León <64605783+JohanLeon0914@users.noreply.github.com>
2024-02-28 20:43:38 -0500

    fixing bug in CardPlayButton hat did not allow a song to start automatically and left the time of the previously played song

--- src/components/CardPlayButton.jsx
+++ src/components/CardPlayButton.jsx
@@ -2,36 +2,40 @@ import { usePlayerStore } from "../store/playerStore";
 import { Pause, Play } from "./Player";
 
 export function CardPlayButton({ id, size = "small" }) {
-  const { currentMusic, setIsPlaying, setCurrentMusic, setIsPlayingSong, isPlayingSong, currentSong, } =
-    usePlayerStore((state) => state);
+  const {
+    currentMusic,
+    setIsPlaying,
+    setCurrentMusic,
+    setIsPlayingSong,
+    isPlayingSong,
+    currentSong,
+  } = usePlayerStore((state) => state);
   const isPlayingPlaylist = isPlayingSong && currentMusic?.playlist.id === id;
 
   const handleClick = () => {
-    if(!currentMusic) {
+    if (!currentMusic) {
       return;
     }
-    if (isPlayingSong) {
+    if (isPlayingSong && isPlayingPlaylist) {
       setIsPlaying(false);
       setIsPlayingSong(false);
       return;
-    }
-
+    } 
     fetch(`/api/get-info-playlist.json?id=${id}`)
       .then((res) => res.json())
       .then((data) => {
         const { songs, playlist } = data;
         setIsPlayingSong(true);
         setIsPlaying(true);
-        if(currentSong) {
+        if (currentMusic?.playlist.id === id) {
           setCurrentMusic({ songs, playlist, song: currentSong });
         } else {
           setCurrentMusic({ songs, playlist, song: songs[0] });
         }
-        
       });
   };
 
-  const iconClassName = size === 'small' ? 'w-4 h-4' : 'w-5 h-5'
+  const iconClassName = size === "small" ? "w-4 h-4" : "w-5 h-5";
 
   return (
     <button
