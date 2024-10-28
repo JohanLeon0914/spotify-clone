833a3aeb460775e1422819d794dafa28e1de3df1
Johan León <64605783+JohanLeon0914@users.noreply.github.com>
2024-10-27 19:18:12 -0500

    Updating aws credentials

--- src/lib/s3.js
+++ src/lib/s3.js
@@ -1,11 +1,10 @@
 import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
 
-// Crear el cliente S3 usando las variables de entorno de Astro
 const s3Client = new S3Client({
-  region: import.meta.env.ASTRO_AWS_REGION,
+  region: "us-west-2",
   credentials: {
-    accessKeyId: import.meta.env.ASTRO_AWS_ACCESS_KEY_ID,
-    secretAccessKey: import.meta.env.ASTRO_AWS_SECRET_ACCESS_KEY,
+    accessKeyId: 'AKIA45PTHCAXJ7S36X36',
+    secretAccessKey: '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',
   },
 });
 
@@ -14,19 +13,18 @@ export const uploadMP3 = async (file, playlist) => {
   const formattedPlaylist = playlist.replace(/ /g, '+');
 
   const params = {
-    Bucket: import.meta.env.ASTRO_AWS_BUCKET,
+    Bucket: 'cdk-hnb659fds-assets-887957688366-us-west-2',
     Key: `${playlist}/${file.name}`,
     Body: file,
     ContentType: file.type,
     ACL: 'public-read',
-    ServerSideEncryption: 'AES256',
   };
 
   try {
     const command = new PutObjectCommand(params);
     const data = await s3Client.send(command);
     console.log('Archivo subido correctamente:', data);
-    return `https://${import.meta.env.ASTRO_AWS_BUCKET}.s3.${import.meta.env.ASTRO_AWS_REGION}.amazonaws.com/${formattedPlaylist}/${file.name}`;
+    return `https://cdk-hnb659fds-assets-887957688366-us-west-2.s3.us-west-2.amazonaws.com/${formattedPlaylist}/${file.name}`;
   } catch (error) {
     console.error('Error al subir el archivo a S3:', error);
     throw new Error('Error al subir el archivo a S3');
